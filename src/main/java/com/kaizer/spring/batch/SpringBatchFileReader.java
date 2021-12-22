d8e6399487692e3e26de6abfc63434a0314fe79d
pk71tmw <86616176+pk71tmw@users.noreply.github.com>
2021-12-22 20:20:14 +0530

    Initial Draft Commit

---
+++ src/main/java/com/kaizer/spring/batch/SpringBatchFileReader.java
@@ -0,0 +1,12 @@
+package com.kaizer.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchFileReader {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchFileReader.class, args);
+	}
+}
